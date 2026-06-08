c940212d6ac3aea7a96f85ff3ead48f0e611cdc3
veit <veit@cusy.io>
2026-06-08 14:01:41 +0200

    :page_facing_up: Add license information
    * Add BSD 3-clause license
    * Add reuse pre-commit check

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 cusy GmbH
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
